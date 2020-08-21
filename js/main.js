67bb04af86cb29eaea2d27d973116b6b91c34297
Deepa Baby <deepababy91@gmail.com>
2020-08-21 15:45:40 -0500

    Adding js file

---
+++ js/main.js
@@ -0,0 +1,25 @@
+
+
+
+$(document).ready(function () {
+  $('.fa-search').click(() => {
+    $('#outputarea').empty();
+    let userInput = $('#search').val();
+
+    $.ajax({
+      type: "GET",
+      url: `http://api.giphy.com/v1/gifs/search?api_key=2T1BqAMfHkOdSwlIDsUmWOmnnuAZcNj8&q=${userInput}&limit=20`
+    })
+      .done(function (res) {
+        let giphy = res.data;
+
+        $.each(giphy, function (i, e) {
+          let gifs = e.images.original.url;
+          console.log("index", i, "element", e.images.original.url)
+          $('#outputarea').append(`<img src="${gifs}" alt="giphy"/>`)
+
+        })
+
+      });
+  });
+});
