d5d00793f3add451a6f395b8d110549c25f7c811
Deepa Baby <deepababy91@gmail.com>
2020-11-14 14:02:03 -0600

    commenting fail function and empty()

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
-//giphy search using $.get
 
+//
 $(document).ready(function () {
   console.log("ready!");
   $('.fa-search').on('click', () => {
+    //using empty() to clear the searchresultarea everytime the user search for a new word 
     $('#outputarea').empty();
     let userInput = $('#search').val();
     $.get(`https://api.giphy.com/v1/gifs/search?api_key=2T1BqAMfHkOdSwlIDsUmWOmnnuAZcNj8&q=${userInput}&limit=30&rating=g`, function (giphyResponse) {
@@ -17,9 +18,9 @@ $(document).ready(function () {
         $('#outputarea').append(imgHtml);
 
       }
-    
+   //fail function to alert an error message whenever the get URL goes wrong (for instance any typo in the URL )
   }).fail(function(){
-      alert('An error occured');
+      alert('An error occured in the get URL');
   });
 
 })
